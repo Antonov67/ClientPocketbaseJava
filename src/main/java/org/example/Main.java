ce18983e31dd316f6399bff2efcc21291b32d8dc
Dm Ant <Dm@Ant.ru>
2025-04-01 16:24:36 +0300

    UI для вывода списка классов

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,14 @@
 package org.example;
 
 import org.example.presentation.MainForm;
-import org.example.service.RetrofitService;
-import org.example.service.SimpleDataCallback;
 
 public class Main {
 
-    private static RetrofitService retrofitService;
     private static MainForm mainForm;
 
     public static void main(String[] args) {
 
-        retrofitService = new RetrofitService();
         mainForm = new MainForm();
-
-        retrofitService.getAllClassrooms(
-                new SimpleDataCallback<ResponseByClass>() {
-            @Override
-            public void load(ResponseByClass data) {
-                for (Classroom classroom : data.items){
-                    System.out.println(classroom.getName());
-                }
-            }
-        });
-
 
     }
 }
--- src/main/java/org/example/presentation/MainForm.java
+++ src/main/java/org/example/presentation/MainForm.java
@@ -1,33 +1,62 @@
 package org.example.presentation;
 
 import org.example.Classroom;
+import org.example.ResponseByClass;
 import org.example.service.RetrofitService;
 import org.example.service.SimpleDataCallback;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainForm extends JFrame {
 
     private static RetrofitService retrofitService;
+    private static List<String> classrooms = new ArrayList<>();
 
     public MainForm(){
 
         retrofitService = new RetrofitService();
 
-        setBounds(100,100,600,400);
+        setBounds(100,100,600,600);
         setLayout(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-
         JTextField classroomField = new JTextField();
-        classroomField.setBounds(10,10,150,50);
+        classroomField.setBounds(10,10,250,50);
         add(classroomField);
 
         JButton addButton = new JButton("добавить класс");
-        addButton.setBounds(10,80, 150,50);
+        addButton.setBounds(10,80, 250,50);
         add(addButton);
+
+        JButton resetButton = new JButton("обновить");
+        resetButton.setBounds(10,150,250,50);
+        add(resetButton);
+
+        JList list = new JList();
+        JScrollPane scrollPane = new JScrollPane(list);
+        scrollPane.setBounds(10, 220, 250, 150);
+        add(scrollPane);
+
+        DefaultListModel<String> model = new DefaultListModel<>();
+
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                retrofitService.getAllClassrooms(new SimpleDataCallback<ResponseByClass>() {
+                    @Override
+                    public void load(ResponseByClass data) {
+                        for (Classroom c : data.items){
+                            model.addElement(c.getName());
+                        }
+                        list.setModel(model);
+                    }
+                });
+            }
+        });
 
         addButton.addActionListener(new ActionListener() {
             @Override
@@ -37,7 +66,7 @@ public class MainForm extends JFrame {
                     retrofitService.createClassroom(classroom, new SimpleDataCallback<Classroom>() {
                         @Override
                         public void load(Classroom data) {
-                            addButton.setText(data.getName());
+                            addButton.setText("Класс " + data.getName() + " добавлен");
                         }
                     });
                 }
